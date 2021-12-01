9da870d8c4976c4bd57d696e9d3ff3eacede38e2
xovest <79008917+xovest@users.noreply.github.com>
2021-12-01 21:59:21 +0200

    seasons

--- src/pages/Sezoane/Sezoane.js
+++ src/pages/Sezoane/Sezoane.js
@@ -1,32 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './Sezoane.css';
-import './script.js';
 import './select.css';
+import Select from './select.js';
 
 const Sezoane = () => {
+    useEffect(() => {
+        const selectElements = document.querySelectorAll('[data-custom]');
+
+        selectElements.forEach(selectElement => {
+            new Select(selectElement);
+        });
+    }, [])
+
     return (
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
-        >
-            <div className="pill">
-                <label>Sezonul</label>
-                <select data-custom>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                </select>
+        <>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <div className="pill">
+                    <label>Sezonul</label>
+                    <select data-custom>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                    </select>
+                </div>
             </div>
-        </div>
+            <div class="main-info-sez">
+                Detalii despre sezon
+            </div>
+            <div class="grid-container-sez">
+                <div class="grid-item-sez">Concurenti</div>
+                <div class="grid-item-sez">Amulete</div>
+                <div class="grid-item-sez">Battle-uri</div>
+                <div class="grid-item-sez">Rețete</div>
+            </div>
+        </>
     );
 };
 
