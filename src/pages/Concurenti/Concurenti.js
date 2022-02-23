0b819a2874e6ba815cf0ac720280c14bd8480ad8
xovest <79008917+xovest@users.noreply.github.com>
2022-02-23 23:29:59 +0200

    basic idea for the conc

--- src/pages/Concurenti/Concurenti.js
+++ src/pages/Concurenti/Concurenti.js
@@ -10,11 +10,122 @@ function Concurenti() {
                 height: '100vh'
             }}
         >
-           <h1>Toti concurentii</h1> 
+            <div
+                style={{
+                    width: 1440,
+                    height: 940,
+                    position: "relative",
+                    overflow: "hidden",
+                }}
+                >
+                <svg
+                    width={1000}
+                    height={2}
+                    viewBox="0 0 1000 2"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    style={{ position: "absolute", left: 219, top: 123 }}
+                    preserveAspectRatio="none"
+                >
+                    <path d="M0 1H1000" stroke="black" />
+                </svg>
+                <p
+                    style={{
+                    width: 694,
+                    height: 121,
+                    position: "absolute",
+                    left: 373,
+                    top: -40,
+                    fontSize: 72,
+                    textAlign: "center",
+                    color: "#000",
+                    }}
+                >
+                    Sezonul 1
+                </p>
+                <div
+                    style={{
+                    width: 300,
+                    height: 300,
+                    position: "absolute",
+                    left: 29,
+                    top: 186,
+                    background: "#c4c4c4",
+                    }}
+                />
+                <div
+                    style={{
+                    width: 300,
+                    height: 300,
+                    position: "absolute",
+                    left: 29,
+                    top: 546,
+                    background: "#c4c4c4",
+                    }}
+                />
+                <div
+                    style={{
+                    width: 300,
+                    height: 300,
+                    position: "absolute",
+                    left: 389,
+                    top: 186,
+                    background: "#c4c4c4",
+                    }}
+                />
+                <div
+                    style={{
+                    width: 300,
+                    height: 300,
+                    position: "absolute",
+                    left: 389,
+                    top: 546,
+                    background: "#c4c4c4",
+                    }}
+                />
+                <div
+                    style={{
+                    width: 300,
+                    height: 300,
+                    position: "absolute",
+                    left: 749,
+                    top: 186,
+                    background: "#c4c4c4",
+                    }}
+                />
+                <div
+                    style={{
+                    width: 300,
+                    height: 300,
+                    position: "absolute",
+                    left: 749,
+                    top: 546,
+                    background: "#c4c4c4",
+                    }}
+                />
+                <div
+                    style={{
+                    width: 300,
+                    height: 300,
+                    position: "absolute",
+                    left: 1109,
+                    top: 186,
+                    background: "#c4c4c4",
+                    }}
+                />
+                <div
+                    style={{
+                    width: 300,
+                    height: 300,
+                    position: "absolute",
+                    left: 1109,
+                    top: 546,
+                    background: "#c4c4c4",
+                    }}
+                />
+            </div>
         </div>
     );
 };
 
 export default Concurenti;
-
-// need to do some of this tho for some modifz
