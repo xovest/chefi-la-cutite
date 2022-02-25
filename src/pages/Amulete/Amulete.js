80f526156a66f12f1fe24f595770ee633a19a18a
xovest <79008917+xovest@users.noreply.github.com>
2022-02-25 23:47:18 +0200

    gotta make them even squarez

--- src/pages/Amulete/Amulete.js
+++ src/pages/Amulete/Amulete.js
@@ -4,13 +4,53 @@ function Amulete() {
     return (
         <div
             style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh'
-            }}
+            width: "auto",
+            height: 1024,
+            position: "relative",
+            overflow: "hidden",
+            background: "blue",
+        }}
         >
-           <h1>Toate amuletele</h1> 
+        <div
+            style={{
+            width: 710,
+            height: 500,
+            position: "absolute",
+            left: "-1px",
+            top: "-1px",
+            background: "#c4c4c4",
+            }}
+        />
+        <div
+            style={{
+            width: 710,
+            height: 500,
+            position: "absolute",
+            left: 927,
+            top: "-1px",
+            background: "#c4c4c4",
+            }}
+        />
+        <div
+            style={{
+            width: 710,
+            height: 505,
+            position: "absolute",
+            left: "-1px",
+            top: 523,
+            background: "#c4c4c4",
+            }}
+        />
+        <div
+            style={{
+            width: 710,
+            height: 500,
+            position: "absolute",
+            left: 937,
+            top: 524,
+            background: "#c4c4c4",
+            }}
+        />
         </div>
     );
 };
