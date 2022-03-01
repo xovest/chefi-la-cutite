81cdbb398ccbc67837f2dd46ac0060abf04ad982
xovest <79008917+xovest@users.noreply.github.com>
2022-03-01 21:57:10 +0200

    one more

--- src/pages/Battleuri/Battleuri.js
+++ src/pages/Battleuri/Battleuri.js
@@ -2,16 +2,56 @@ import React from 'react'
 
 function Battleuri() {
     return (
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh'
+            <div
+                style={{
+                width: 1648,
+                height: 1024,
+                position: "relative",
+                overflow: "hidden",
+                background: "#fff",
             }}
-        >
-           <h1>Toate Battleurile</h1> 
-        </div>
+            >
+            <div
+                style={{
+                width: 200,
+                height: 200,
+                position: "absolute",
+                left: "-1px",
+                top: "-1px",
+                background: "#c4c4c4",
+                }}
+            />
+            <div
+                style={{
+                width: 200,
+                height: 824,
+                position: "absolute",
+                left: "-1px",
+                top: 199,
+                background: "#6d6d6d",
+                }}
+            />
+            <div
+                style={{
+                width: 1680,
+                height: 200,
+                position: "absolute",
+                left: 199,
+                top: "-1px",
+                background: "#797979",
+                }}
+            />
+            <div
+                style={{
+                width: 1680,
+                height: 824,
+                position: "absolute",
+                left: 199,
+                top: 199,
+                background: "#c4c4c4",
+                }}
+            />
+            </div>
     );
 };
 
